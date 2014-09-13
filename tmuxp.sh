701dd416b0394b13e95c1eb7685f1986b1e969b1
Ge Wang <code.gewang@gmail.com>
2014-09-13 17:46:43 -0600

    Simplify tmuxp.sh

--- tmuxp.sh
+++ tmuxp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tmux attach-session -t ${1?} >/dev/null 2>&1 ||
-tmuxp load ~/.tmuxp/${1}.yaml
+tmux_quiet_attach.sh ${1?} ||
+tmuxp load $(find ~/etc*/tmuxp -type f -name ${1}.yaml | head -n1)
