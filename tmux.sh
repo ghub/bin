afd20f150eaa685b28786a3930147a0091b7219e
Ge Wang <code.gewang@gmail.com>
2014-09-13 14:28:48 -0600

    Refactor tmux setup

--- tmux.sh
+++ tmux.sh
@@ -1,22 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
-project=devel
-project_dir=~/ws/$project
-
-tmux has-session -t $project
-
-if [ $? != 0 ]; then
-
-    tmux new-session    -s $project -n editor -d
-    tmux send-keys      -t $project                "cd $project_dir" C-m
-    tmux send-keys      -t $project                "vim" C-m
-    tmux split-window   -t $project -v
-    tmux select-layout  -t $project main-horizontal
-    tmux send-keys      -t $project:1.2            "cd $project_dir" C-m
-    tmux new-window     -t $project -n console
-    tmux send-keys      -t $project:2              "cd $project_dir" C-m
-    tmux select-window  -t $project:1
-
-fi
-
-tmux attach-session -t $project
+tmux attach-session -t ${1?} >/dev/null 2>&1 ||
+tmux source-file $(find ~/etc* -type f -name ${1}.tmux | head -n1)
