b8ffe654d35c777e1687fa3d1a7bc5144e36c69c
Ge Wang <code.gewang@gmail.com>
2014-09-13 14:50:43 -0600

    Simplify tmuxp.sh

--- tmuxp.sh
+++ tmuxp.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-project=${1?}
-
-tmux has-session -t $project > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-    tmux attach-session -t $project
-else
-    tmuxp load ~/.tmuxp/${project}.yaml
-fi
+tmux attach-session -t ${1?} >/dev/null 2>&1 ||
+tmuxp load ~/.tmuxp/${1}.yaml
