7af8473a0958cdb35847feece6426d9f48d94214
Ge Wang <code.gewang@gmail.com>
2014-09-28 15:23:41 -0600

    Replace j2 with z to avoid race

--- clone_repos.sh
+++ clone_repos.sh
@@ -4,6 +4,6 @@ cd ~/repo
 
 github_clone_ro.sh gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 github_clone_ro.sh krzysztof-jusiak/gmock
-github_clone_ro.sh rupa/j2
+github_clone_ro.sh rupa/z
 github_clone_ro.sh sjl/peat
 github_clone_ro.sh sjl/t
