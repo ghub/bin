9bab82b8432eb5f36b0c8712ca45cc698baac512
Ge Wang <code.gewang@gmail.com>
2014-10-10 15:45:12 -0600

    Exclude Arm includes

--- update_includes.sh
+++ update_includes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 (
-find * -name "*.h" -o -name "*.moc" | xargs dirname | grep -vw win
+find * -name "*.h" -o -name "*.moc" | xargs dirname | grep -Evw "arm|win"
 find * -name include
 ) |
 sort -u > .includes
