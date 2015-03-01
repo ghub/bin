7a23082d9947ac5f52ac826e94c1f2642c5b6e0e
Ge Wang <code.gewang@gmail.com>
2015-03-01 12:18:23 -0700

    Move tmux sessions

--- tmux.sh
+++ tmux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 tmux_quiet_attach.sh ${1?} ||
-tmux start-server \; source-file $(find ~/etc*/tmux.session -type f -name ${1}.tmux | head -n1)
+tmux start-server \; source-file $(find ~/etc*/tmux.d -type f -name ${1}.tmux | head -n1)
--- tmuxp.sh
+++ tmuxp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 tmux_quiet_attach.sh ${1?} ||
-tmuxp load $(find ~/etc*/tmuxp -type f -name ${1}.yaml | head -n1)
+tmuxp load $(find ~/etc*/tmuxp.d -type f -name ${1}.yaml | head -n1)
