46c972df1b3ae354d1f14cf03c3b14e6299a7aae
Ge Wang <code.gewang@gmail.com>
2015-06-28 22:30:50 -0600

    Allow project .schrootrc

--- schroot.sh
+++ schroot.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -e
 
+git_root=$( git_root.sh )
+
 if [ -f .schrootrc ]; then
     name=$( cat .schrootrc )
+elif [ -f $git_root/.schrootrc ]; then
+    name=$( cat $git_root/.schrootrc )
 elif [ -f ~/.schrootrc ]; then
     name=$( cat ~/.schrootrc )
 else
