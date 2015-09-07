2c795b27b7f19b5ac71c1045c0a1e2420ae6ad24
Ge Wang <code.gewang@gmail.com>
2015-09-07 08:17:18 -0600

    Add GLSL validator and viewer

--- clone_repos.sh
+++ clone_repos.sh
@@ -2,9 +2,12 @@
 
 cd ~/repo
 
+github_clone_ro.sh apitrace/apitrace
 github_clone_ro.sh gittup/tup
 github_clone_ro.sh gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
+github_clone_ro.sh KhronosGroup/glslang
 github_clone_ro.sh krzysztof-jusiak/gmock
+github_clone_ro.sh patriciogonzalezvivo/glslViewer
 github_clone_ro.sh rupa/z
 github_clone_ro.sh sjl/peat
 github_clone_ro.sh sjl/t
