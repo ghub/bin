6f584989e4c7949e0341e652d3a323af3a628903
Ge Wang <code.gewang@gmail.com>
2016-01-29 19:45:19 -0700

    Install Intel 7260

--- wifi.sh
+++ wifi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-sudo wpa_supplicant -B -D nl80211 -i wlan0 -c /etc/wpa_supplicant.conf
-sudo dhclient wlan0
+sudo wpa_supplicant -B -D nl80211 -i wlp3s0 -c /etc/wpa_supplicant.conf
+sudo dhclient wlp3s0
