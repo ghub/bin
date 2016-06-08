2ff0f3baa62aef94bb464a696bd39e9d5e88131e
Ge Wang <code.gewang@gmail.com>
2016-06-08 10:53:55 -0600

    Use bundled clang on Linux as well

--- build_ycm.sh
+++ build_ycm.sh
@@ -1,10 +1,3 @@
 #!/bin/sh -e
 
-cmd="$HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer"
-
-if [ $( uname -s ) = Darwin ]; then
-    $cmd
-else
-    # Allow ldconfig to find python library
-    PATH=/sbin:$PATH $cmd --system-libclang
-fi
+~/.vim/bundle/YouCompleteMe/install.py --clang-completer
