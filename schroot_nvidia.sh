8b18ca35e881bf0c8189c628f042fb002bcd15c7
Ge Wang <code.gewang@gmail.com>
2016-12-20 13:10:21 -0700

    Upgrade NVidia driver to 375.20

--- schroot_nvidia.sh
+++ schroot_nvidia.sh
@@ -3,10 +3,12 @@
 ./nvidia-installer \
     --accept-license \
     --no-distro-scripts \
+    --no-glvnd-egl-client \
+    --no-glvnd-glx-client \
+    --no-install-libglvnd \
     --no-kernel-module \
     --no-kernel-module-source \
     --no-nvidia-modprobe \
     --no-precompiled-interface \
-    --no-runlevel-check \
     --no-x-check \
 
