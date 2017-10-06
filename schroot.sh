32b0149efe75f817276d8b22cdbb77891ab33d29
Ge Wang <code.gewang@gmail.com>
2017-10-06 12:19:56 -0600

    Fix schroot.sh to use base name when begin session

--- schroot.sh
+++ schroot.sh
@@ -2,6 +2,7 @@
 
 chroot_name=$( schroot_chroot_name.sh )
 session_name=$( schroot_session_name.sh )
+base_session_name=$( echo $session_name | sed -e s/.*:// )
 pre=$( schroot_pre.sh )
 
 if [ $# -gt 0 ]; then
@@ -11,6 +12,6 @@ else
 fi
 
 schroot --list --all-sessions | grep -qw $session_name ||
-schroot --begin-session --chroot $chroot_name --session-name $session_name
+schroot --begin-session --chroot $chroot_name --session-name $base_session_name
 
 exec schroot --run-session --chroot $session_name -- bash -c "$pre && $cmd"
