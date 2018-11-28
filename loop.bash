7584fae5417a6844b18bd034082b0ce57d68a9f7
Ge Wang <code.gewang@gmail.com>
2018-11-28 14:01:07 -0700

    Revise loop.bash

--- loop.bash
+++ loop.bash
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 
-cmd=$1
-end=${2:-10}
+ITERATIONS=$1
+echo "ITERATIONS=$ITERATIONS"
 
-echo "CMD=$cmd"
-echo "END=$end"
+shift
 
-for i in $(seq 1 $end); do
-    echo "======================================== $i/$end"
-    echo "CMD=$cmd"
-    $cmd
+COMMAND="$@"
+echo "COMMAND=$COMMAND"
+
+for i in $(seq 1 $ITERATIONS); do
+    echo "======================================== $i/$ITERATIONS start"
+    echo "COMMAND=$COMMAND"
+    $COMMAND
+    echo "======================================== $i/$ITERATIONS end"
 done
