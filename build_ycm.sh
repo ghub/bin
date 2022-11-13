914adc44067c43277ca012ecc25bc327d7cf1507
Ge Wang <code.gewang@gmail.com>
2022-11-13 11:13:43 -0700

    Update build_ycm.sh

--- build_ycm.sh
+++ build_ycm.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -e
 
-~/.vim/bundle/YouCompleteMe/install.py --clang-completer
+cd ~/.vim/bundle/YouCompleteMe
+build_dir=`pwd`/build
+rm -rf build
+EXTRA_CMAKE_ARGS="-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ./install.py --all --build-dir=$build_dir --verbose
